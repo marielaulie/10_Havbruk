1c2e41f2e7909b2bc3bb7a2a5697292b3c869316
marielaulie <54407324+marielaulie@users.noreply.github.com>
2021-06-16 13:05:20 +0200

    Opprettet app

---
+++ 10_Havbruk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  10_Havbruk
+//
+//  Created by Mariel Aulie Hinderaker on 16/06/2021.
+//  Copyright © 2021 Mariel Aulie Hinderaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
